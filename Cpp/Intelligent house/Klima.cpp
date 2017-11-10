17fa941ea3421ca2e8be86e0da47f6282993a90a
Michnic120 <michnic120@gmail.com>
2017-11-10 09:38:57 +0100

    Create Klima.cpp

---
+++ Cpp/Intelligent house/Klima.cpp
@@ -0,0 +1,31 @@
+#include "Klima.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+CKlima::CKlima()
+{
+  iMojStan = 0; //1-włączony 0-wyłączony
+}
+
+CKlima::~CKlima()
+{
+}
+
+void CKlima::UstawKlimeON()
+{
+  iMojStan = 1;
+}
+
+void CKlima::UstawKlimeOFF()
+{
+  iMojStan = 0;
+}
+
+int CKlima::DajStan()
+{
+  return iMojStan;
+}
