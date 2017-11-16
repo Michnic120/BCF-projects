5552c4e1ddee535bee666f476496d8d6ae84ddfd
Michnic120 <michnic120@gmail.com>
2017-11-16 13:33:44 +0100

    Update Sched.cpp

--- Cpp/Intelligent house/Sched.cpp
+++ Cpp/Intelligent house/Sched.cpp
@@ -1,50 +1,83 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <string>
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <windows.h>
-#include <stdlib.h>
-#include <conio.h>
-
-#include "Pokoj.h"
 #include "Sched.h"
+#include <string.h>
+#include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
 #include "Sterownik.h"
+#include <windows.h>
+#include <conio.h>
+#include "Sterownik.h"
+#include <string>
 
 using namespace std;
 VOID WINAPI Sleep(DWORD dwMilliseconds);
 
-int main()
+CSched::CSched(CPokoj *pokoik1, CSterownik *ster1, CPokoj *pokoik2, CSterownik *ster2, CPokoj *pokoik3, CSterownik *ster3, CPokoj *pokoik4, CSterownik *ster4, CPokoj *pokoik5, CSterownik *ster5, int cykl)
 {
-	vector<CKlima> klima(5);
-	
-	CPokoj  poddasze (&klima[0], 0.1, 0.01),
-            salon    (&klima[1], 0.3, 0.07),
-            sypialnia(&klima[2], 0.3, 0.07),
-            garaz    (&klima[3], 0.3, 0.02),
-            piwnica  (&klima[4], 0.6, 0.02);
+	iCyklCzasu = cykl;
+	pokoj1 = pokoik1;
+	st1 = ster1;
+	pokoj2 = pokoik2;
+	st2 = ster2;
+	pokoj3 = pokoik3;
+	st3 = ster3;
+	pokoj4 = pokoik4;
+	st4 = ster4;
+	pokoj5 = pokoik5;
+	st5 = ster5;
+}
 
-	vector<int> temp(5);
+CSched::~CSched()
+{
+}
 
-	//vector<int>::size_type sz_temp = temp.size();
-	//for(short i=0; i<sz_temp; i++){}
+void CSched::uruchamiacz()
+{
+	for (int i=0;i<=iCyklCzasu;i++) {
+		
+		cout << "           Aktualna temperatura:       Temperatura na sterowniku:" << endl;
+		cout << "Poddasze:                        " << pokoj1->DajTem() << endl << "                                                                  " << st1->DajTemperature() << endl;
+		cout << "Salon:                           " << pokoj2->DajTem() << endl << "                                                                  " << st2->DajTemperature() << endl;
+		cout << "Sypialnia:                       " << pokoj3->DajTem() << endl << "                                                                  " << st3->DajTemperature() << endl;
+		cout << "Garaz:                           " << pokoj4->DajTem() << endl << "                                                                  " << st4->DajTemperature() << endl;
+		cout << "Piwnica:                         " << pokoj5->DajTem() << endl << "                                                                  " << st5->DajTemperature() << endl;
+		
+		pokoj1->ZmienTem(); //dać stan od klimy
+		pokoj2->ZmienTem();
+		pokoj3->ZmienTem();
+		pokoj4->ZmienTem();
+		pokoj5->ZmienTem();
+		
+		if (_kbhit()) {
+			string a;
+			cin >> a;
 
-	cout  << "Podaj temperature w kazdym z pomieszczen" << endl<<endl;
-	cout  << "Poddasze:  "; cin >> temp[0];
-	cout  << "Salon:     "; cin >> temp[1];
-	cout  << "Sypialnia: "; cin >> temp[2];
-	cout  << "Garaz:     "; cin >> temp[3];
-	cout  << "Piwnica:   "; cin >> temp[4];
+			if (a == "poddasze+")st1->ZwiekszTemperatur(); if (a == "poddasze-")st1->ZmiejszTemperature();
+			if (a == "salon+")st2->ZwiekszTemperatur(); if (a == "salon-")st2->ZmiejszTemperature();
+			if (a == "sypialnia+")st3->ZwiekszTemperatur(); if (a == "sypialnia-")st3->ZmiejszTemperature();
+			if (a == "garaz+")st4->ZwiekszTemperatur(); if (a == "garaz-")st4->ZmiejszTemperature();
+			if (a == "piwnica+")st5->ZwiekszTemperatur(); if (a == "piwnica-")st5->ZmiejszTemperature();
 
-	CSterownik  s1(&klima[0], &poddasze,  temp[0]),
-                s2(&klima[1], &salon,     temp[1]),
-                s3(&klima[2], &sypialnia, temp[2]),
-                s4(&klima[3], &garaz,     temp[3]),
-                s5(&klima[4], &piwnica,   temp[4]);
+			if (a == "poddaszeokno+") { pokoj1->plus *= 1.5; cout << "Okno na poddaszu otwarte."; }
+			if (a == "poddaszeokno-") { pokoj1->plus /= 1.5; cout << "Okno na poddaszu zamkniete."; }
+		
+			if (a == "salonokno+"){pokoj2->plus *= 1.5; cout << "Okno w salonie  otwarte.";}
+			if (a == "salonokno-"){pokoj2->plus /= 1.5; cout << "Okno w salonie zamkniete.";}
 
-	CSched shed(&poddasze, &s1, &salon, &s2, &sypialnia, &s3, &garaz, &s4, &piwnica, &s5, 1000);
+			if (a == "sypialniaokno+"){pokoj3->plus *= 1.5; cout << "Okno w sypialni otwarte.";}
+			if (a == "sypialniaokno-"){pokoj3->plus /= 1.5; cout << "Okno w sypialni zamkniete.";}
+		
+		}
+		
+		st1->Kontrola();
+		st2->Kontrola();
+		st3->Kontrola();
+		st4->Kontrola();
+		st5->Kontrola();
+		
+		Sleep(500);
+		system("cls");
+	}
 
-	shed.uruchamiacz();
-
-	return 0;
 }
