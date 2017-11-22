0f9bfd318fe17331cc887e22178c60cb958d294e
Michnic120 <michnic120@gmail.com>
2017-11-22 17:16:24 +0100

    Update Source.cpp

--- Cpp/Intelligent house/Source.cpp
+++ Cpp/Intelligent house/Source.cpp
@@ -20,6 +20,8 @@ int main()
 	vector<CKlima> klima(5);
     vector<CPokoj> pokoje(5);
     vector<int> tem(5);
+	//vector<CSterownik> stery(5);
+	
 	
 	vector<CPokoj>::size_type sz_pok = pokoje.size();
 
@@ -28,9 +30,12 @@ int main()
             pok_buffer.klima1=&klima[i];
             pok_buffer.minus =i/0.1;
             pok_buffer.plus =i/0.01;
+            pok_buffer.numer = i;
             pokoje.push_back(pok_buffer);
         }
         
+        
+/*
 
 	CPokoj  poddasze (&klima[0], 0.1, 0.01),
             salon    (&klima[1], 0.3, 0.07),
@@ -39,8 +44,24 @@ int main()
             piwnica  (&klima[4], 0.6, 0.02);
 
 
-	//vector<int>::size_type sz_tem = tem.size();
-	//for(short i=0; i<sz_tem; i++){}
+
+cout  << "Podaj temperature w kazdym z pomieszczen" << endl<<endl;
+    vector<int>::size_type sz_tem = tem.size();
+	for(short i=0; i<sz_tem; i++)
+	 {
+	    cout << JakisPokoj.nazwa << ":    "; cin >> JakisPokoj.tem; albo jakoś inaczej
+	    
+	 }
+
+vector<CSterownik>::size_type sz_stery = tem.stery();
+for(short i=0; i<sz_stery; i++)
+{
+    stery[i] = (&klima[i], &pokoj[i],  tem[i]),
+              
+}
+
+*/
+
 
 	cout  << "Podaj temperature w kazdym z pomieszczen" << endl<<endl;
 	cout  << "Poddasze:  "; cin >> tem[0];
@@ -49,11 +70,14 @@ int main()
 	cout  << "Garaz:     "; cin >> tem[3];
 	cout  << "Piwnica:   "; cin >> tem[4];
 
+
 	CSterownik  s1(&klima[0], &poddasze,  tem[0]),
                 s2(&klima[1], &salon,     tem[1]),
                 s3(&klima[2], &sypialnia, tem[2]),
                 s4(&klima[3], &garaz,     tem[3]),
                 s5(&klima[4], &piwnica,   tem[4]);
+
+
 
 	CSched shed(&poddasze, &s1, &salon, &s2, &sypialnia, &s3, &garaz, &s4, &piwnica, &s5, 1000);
 
