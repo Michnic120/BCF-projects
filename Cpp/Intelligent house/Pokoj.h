1eed1547126523d8021b2a595a624d9cac55babd
Michnic120 <michnic120@gmail.com>
2018-02-21 11:09:54 +0100

    Update Pokoj.h

--- Cpp/Intelligent house/Pokoj.h
+++ Cpp/Intelligent house/Pokoj.h
@@ -3,16 +3,17 @@ pragma once
 
 class CPokoj
 {
-private:
-  double dAktualnaTem;
-public:
-  CPokoj();
-  CPokoj(CKlima *klimka, float zmn, float zw, short num);
-  ~CPokoj();
+  private:
+    double dAktualnaTem;
+  public:
+    CPokoj();
+    CPokoj(CKlima *klimka, float zmn, float zw, short num);
+    ~CPokoj();
    
   CKlima *klima1;
   short numer;
   float plus, minus;
   void ZmienTem();
   double DajTem();
+
 };
