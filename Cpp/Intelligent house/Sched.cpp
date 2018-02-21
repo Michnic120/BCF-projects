c5b7829b0ec47dc0cc53d4dc8f205045c930350a
Michnic120 <michnic120@gmail.com>
2018-02-21 12:20:55 +0100

    Update Sched.cpp

--- Cpp/Intelligent house/Sched.cpp
+++ Cpp/Intelligent house/Sched.cpp
@@ -4,91 +4,91 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cstdlib>
-#include "Sterownik.h"
+#include "Controller.h"
 #include <windows.h>
 #include <conio.h>
-#include "Sterownik.h"
 #include <string>
 
 using namespace std;
 VOID WINAPI Sleep(DWORD dwMilliseconds);
 
-CSched::CSched(CPokoj *pokoik1, CSterownik *ster1, CPokoj *pokoik2, CSterownik *ster2, CPokoj *pokoik3, CSterownik *ster3, CPokoj *pokoik4, CSterownik *ster4, CPokoj *pokoik5, CSterownik *ster5, int cykl)
+CSched::CSched(CRoom *rom1, CController *cont1, CRoom *rom2, CController *cont2, CRoom *rom3, CController *cont3, CRoom *rom4, CController *cont4, CRoom *rom5, CController *cont5, int cycle)
 {
-	iCyklCzasu = cykl;
+	iTimeCycle = cycle;
 	
-	pokoj1 = pokoik1;
-	st1 = ster1;
+	room1 = rom1;
+	con1 = cont1;
 	
-	pokoj2 = pokoik2;
-	st2 = ster2;
+	room2 = rom2;
+	con2 = cont2;
 	
-	pokoj3 = pokoik3;
-	st3 = ster3;
+	room3 = rom3;
+	con3 = cont3;
 	
-	pokoj4 = pokoik4;
-	st4 = ster4;
+	room4 = rom4;
+	con4 = cont4;
 	
-	pokoj5 = pokoik5;
-	st5 = ster5;
+	room5 = rom5;
+	con5 = cont5;
 }
 
 CSched::~CSched()
 {
 }
 
-void CSched::uruchamiacz()
+void CSched::Starter()
 {
-	for (int i=0;i<=iCyklCzasu;i++) {
+	for (int i=0;i<=iTimeCycle;i++) 
+	{
 		
-		cout << "           Aktualna temperatura:       Temperatura na sterowniku:" << endl;
-		cout << "Poddasze:                        " << pokoj1->DajTem() << endl << "                                                                  " << st1->DajTemperature() << endl;
-		cout << "Salon:                           " << pokoj2->DajTem() << endl << "                                                                  " << st2->DajTemperature() << endl;
-		cout << "Sypialnia:                       " << pokoj3->DajTem() << endl << "                                                                  " << st3->DajTemperature() << endl;
-		cout << "Garaz:                           " << pokoj4->DajTem() << endl << "                                                                  " << st4->DajTemperature() << endl;
-		cout << "Piwnica:                         " << pokoj5->DajTem() << endl << "                                                                  " << st5->DajTemperature() << endl;
+		cout << "           Current temperature:       Temperature on controller:" << endl;
+		cout << "Attic:                             " << room1->GiveTem() << endl << "                                                                  " << st1->DajTemperature() << endl;
+		cout << "Living room:                       " << room2->GiveTem() << endl << "                                                                  " << st2->DajTemperature() << endl;
+		cout << "Bed room:                          " << room3->GiveTem() << endl << "                                                                  " << st3->DajTemperature() << endl;
+		cout << "Garage:                            " << room4->GiveTem() << endl << "                                                                  " << st4->DajTemperature() << endl;
+		cout << "Cellar:                            " << room5->GiveTem() << endl << "                                                                  " << st5->DajTemperature() << endl;
 		
-		pokoj1->ZmienTem(); //dać stan od klimy
-		pokoj2->ZmienTem();
-		pokoj3->ZmienTem();
-		pokoj4->ZmienTem();
-		pokoj5->ZmienTem();
+		room1->ChangeTem(); 
+		room2->ChangeTem();
+		room3->ChangeTem();
+		room4->ChangeTem();
+		room5->ChangeTem();
 		
 		if (_kbhit()) {
 			string a;
 			cin >> a;
 
-			if (a == "poddasze+")st1->ZwiekszTemperatur(); 
-			if (a == "poddasze-")st1->ZmiejszTemperature();
+			if (a == "attic+")con1->IncreaseTemp(); 
+			if (a == "attic-")con1->DecreaseTemp();
 			
-			if (a == "salon+")st2->ZwiekszTemperatur(); 
-			if (a == "salon-")st2->ZmiejszTemperature();
+			if (a == "livroom+")con2->IncreaseTemp(); 
+			if (a == "livroom-")con2->DecreaseTemp();
 			
-			if (a == "sypialnia+")st3->ZwiekszTemperatur(); 
-			if (a == "sypialnia-")st3->ZmiejszTemperature();
+			if (a == "bedroom+")con3->IncreaseTemp(); 
+			if (a == "bedroom-")con3->DecreaseTemp();
 			
-			if (a == "garaz+")st4->ZwiekszTemperatur(); 
-			if (a == "garaz-")st4->ZmiejszTemperature();
+			if (a == "garage+")con4->IncreaseTemp(); 
+			if (a == "garage-")con4->DecreaseTemp();
 			
-			if (a == "piwnica+")st5->ZwiekszTemperatur(); 
-			if (a == "piwnica-")st5->ZmiejszTemperature();
+			if (a == "cellar+")con5->IncreaseTemp(); 
+			if (a == "cellar-")con5->DecreaseTemp();
 
-			if (a == "poddaszeokno+") { pokoj1->plus *= 1.5; cout << "Okno na poddaszu otwarte."; }
-			if (a == "poddaszeokno-") { pokoj1->plus /= 1.5; cout << "Okno na poddaszu zamkniete."; }
+			if (a == "atticwindow+") { room1->plus *= 1.5; cout << "Window in attic opened."; }
+			if (a == "atticwindow-") { room1->plus /= 1.5; cout << "Window in attic closed."; }
 		
-			if (a == "salonokno+"){pokoj2->plus *= 1.5; cout << "Okno w salonie  otwarte.";}
-			if (a == "salonokno-"){pokoj2->plus /= 1.5; cout << "Okno w salonie zamkniete.";}
+			if (a == "livroomwindow+"){room2->plus *= 1.5; cout << "Window in living room opened.";}
+			if (a == "livroomwindow-"){room2->plus /= 1.5; cout << "Window in living room closed.";}
 
-			if (a == "sypialniaokno+"){pokoj3->plus *= 1.5; cout << "Okno w sypialni otwarte.";}
-			if (a == "sypialniaokno-"){pokoj3->plus /= 1.5; cout << "Okno w sypialni zamkniete.";}
+			if (a == "bedroomwindow+"){room3->plus *= 1.5; cout << "Window in bed room opened.";}
+			if (a == "bedroomwindow-"){room3->plus /= 1.5; cout << "Window in bed room closed.";}
 		
 		}
 		
-		st1->Kontrola();
-		st2->Kontrola();
-		st3->Kontrola();
-		st4->Kontrola();
-		st5->Kontrola();
+		con1->Control();
+		con2->Control();
+		con3->Control();
+		con4->Control();
+		con5->Control();
 		
 		Sleep(500);
 		system("cls");
