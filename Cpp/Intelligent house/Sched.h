ed97a39bd34b60b1a0795c47631a6bfd088388f3
Michnic120 <michnic120@gmail.com>
2018-02-21 11:12:13 +0100

    Update Sched.h

--- Cpp/Intelligent house/Sched.h
+++ Cpp/Intelligent house/Sched.h
@@ -4,14 +4,16 @@
 
 class CSched
 {
-private:
-  int iCyklCzasu;
-public:
-  CPokoj* pokoj1, *pokoj2, *pokoj3, *pokoj4, *pokoj5;
-  CSterownik *st1, *st2, *st3, *st4, *st5;
-  CSched(CPokoj *pokoik1, CSterownik *ster1, CPokoj *pokoik2, CSterownik *ster2, CPokoj
-  *pokoik3, CSterownik *ster3, CPokoj *pokoik4, CSterownik *ster4, CPokoj *pokoik5, CSterownik
-  *ster5, int cykl);
-  ~CSched();
-  void uruchamiacz();
+  private:
+    int iCyklCzasu;
+
+  public:
+    CPokoj* pokoj1, *pokoj2, *pokoj3, *pokoj4, *pokoj5;
+    CSterownik *st1, *st2, *st3, *st4, *st5;
+    CSched(CPokoj *pokoik1, CSterownik *ster1, CPokoj *pokoik2, CSterownik *ster2, 
+           CPokoj *pokoik3, CSterownik *ster3, CPokoj *pokoik4, CSterownik *ster4, 
+           CPokoj *pokoik5, CSterownik *ster5, int cykl);
+    ~CSched();
+  
+    void uruchamiacz();
 };
