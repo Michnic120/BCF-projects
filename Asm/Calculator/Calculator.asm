b9d6f3f8b7b38258632182e07277c1f863cd24f6
michniew <krzysztof.michniewicz@nokia.com>
2019-11-25 10:47:12 +0100

    translation

--- Asm/Calculator/Calculator.asm
+++ Asm/Calculator/Calculator.asm
@@ -1,19 +1,19 @@
-PROJEKT PRZYCISK 2
+PROJECT CALC
 
-;Port A i C do wyświatlacza
-;Port B do klawiatury
-;Port D do buzzera
-;buzzer połączony z PIND0 i GND
+;Port A & C for display
+;Port B for keyboard
+;Port D for buzzer
+;buzzer connected to PIND0 i GND
 
 .include "m32def.inc"
-.def Liczba = R16
-.def Wynik = R17
-.def Flaga = R18
-.def Znak = R21
-.def WSegment = R22
-.def WKolumna = R23
-.def KWiersz = R24
-.def KKolumna = R25
+.def Number = R16
+.def Result = R17
+.def Flag = R18
+.def Sign = R21
+.def WRow = R22
+.def WColumn = R23
+.def KRow = R24
+.def KColumn = R25
 
 .cseg
 .org 0x0000
@@ -22,320 +22,320 @@ jmp Start
 
 Start:
 
-;KONFIGURACJA: PORT A i C STERUJE SEGMENTAMI i KOLUMNAMI WYŚWIETLACZA
-ldi WSegment, 0xFF
-out DDRA, WSegment
-ldi WKolumna, 0xFF
-out DDRC, WKolumna
+;CONFIGURATION: PORT A & C CONTROLS  ROWS AND COLUMNS OF THE DISPLAY
+ldi WRow, 0xFF
+out DDRA, WRow
+ldi WColumn, 0xFF
+out DDRC, WColumn
 
-;konfiguracja portu D do sterowania buzzerem
-ldi WSegment, 0x01
-out DDRD, WSegment
+;CONFIGURATION: PORT D to control buzzer
+ldi WRow, 0x01
+out DDRD, WRow
 
 
-Klawiatura:
+Keyboard:
 
-ldi R30, HIGH(RAMEND)	; inicjalicja 16-bitowego stosu
-out SPH, R30			
-ldi R31, LOW(RAMEND)	
+ldi R30, HIGH(RAMEND)	; 16-bit stack initialization
+out SPH, R30
+ldi R31, LOW(RAMEND)
 out SPL, R31
 
 
-ldi KWiersz, 0xF0		;Sprawdzenie, w którym wierszu został wciśnięty przycisk
-out DDRB, KWiersz		;w przypadku przyciśnięcia przejdź do obsługi przerwania włączającego buzzer na czas zwarcia 
-ldi KWiersz, 0x0F
-out PORTB, KWiersz
+ldi KRow, 0xF0
+out DDRB, KRow
+ldi KRow, 0x0F
+out PORTB, KRow
 
-sbis PINB, 0  ldi KWiersz, 6	sbis PINB, 0 rcall BUZZ_on 
-sbis PINB, 1  ldi KWiersz, 3	sbis PINB, 1 rcall BUZZ_on 
-sbis PINB, 2  ldi KWiersz, 0	sbis PINB, 2 rcall BUZZ_on
-sbis PINB, 3  ldi KWiersz, 9	sbis PINB, 3 rcall BUZZ_on
-
-
-ldi KKolumna, 0x0F		;Sprawdzenie, w której kolumnie został wciśnięty przycisk
-out DDRB, KKolumna
-ldi KKolumna, 0xF0
-out PORTB, KKolumna
-
-sbis PINB, 4 ldi KKolumna, 1	sbis PINB, 4 rcall BUZZ_on
-sbis PINB, 5 ldi KKolumna, 2	sbis PINB, 5 rcall BUZZ_on
-sbis PINB, 6 ldi KKolumna, 3	sbis PINB, 6 rcall BUZZ_on
-sbis PINB, 7 ldi KKolumna, 13	sbis PINB, 7 rcall BUZZ_on
+sbis PINB, 0  ldi KRow, 6	sbis PINB, 0 rcall BUZZ_on
+sbis PINB, 1  ldi KRow, 3	sbis PINB, 1 rcall BUZZ_on
+sbis PINB, 2  ldi KRow, 0	sbis PINB, 2 rcall BUZZ_on
+sbis PINB, 3  ldi KRow, 9	sbis PINB, 3 rcall BUZZ_on
 
 
-add KWiersz,KKolumna	; przepisuje do rejestru R24 Wynik klawiatury
+ldi KColumn, 0x0F
+out DDRB, KColumn
+ldi KColumn, 0xF0
+out PORTB, KColumn
 
-cpi KWiersz, 11			; jeśli +,-,/,*  to przepisz
-brlt Liczbaprog
-cpi KWiersz, 13
-
-
-
-Liczbaprog:
-
-mov Liczba, KWiersz
-brsh Rownasie
+sbis PINB, 4 ldi KColumn, 1	 sbis PINB, 4 rcall BUZZ_on
+sbis PINB, 5 ldi KColumn, 2	 sbis PINB, 5 rcall BUZZ_on
+sbis PINB, 6 ldi KColumn, 3	 sbis PINB, 6 rcall BUZZ_on
+sbis PINB, 7 ldi KColumn, 13 sbis PINB, 7 rcall BUZZ_on
 
 
-Znakprog:
-mov Znak, KWiersz
-rjmp Klawiatura
+add KRow,KColumn
+
+cpi KRow, 11
+brlt Numberprog
+cpi KRow, 13
 
 
-Rownasie:				;jeśli = to do klawiatury 
-cpi KWiersz, 12
-rjmp Kalkulator
+Numberprog:
 
-Kalkulator:
-Petla0:
-brne Petla1
-//rjmp Cyfra
+mov Number, KRow
+brsh Equals
 
 
-Petla1:
-cpi Znak, 22		;dodanie
-brne Petla2
-rjmp Dodaj
-
-Petla2:
-cpi Znak, 13		;odejmowanie
-brne Petla3
-rjmp Odejmij
-
-Petla3:
-cpi Znak, 16		;mnożenie
-rjmp Mnozenie
+Signprog:
+mov Sign, KRow
+rjmp Keyboard
 
 
-Dodaj:
-add Wynik, Liczba
-rcall Wyswietl
+Equals:
+cpi KRow, 12
+rjmp Calculator
 
-Odejmij:
-sub Wynik, Liczba
-rcall Wyswietl
-
-Mnozenie:
-mul Wynik, Liczba
-rcall Wyswietl
+Calculator:
+Loop0:
+brne Loop1
+//rjmp Digit
 
 
+Loop1:
+cpi Sign, 22		; Add
+brne Loop2
+rjmp Add
 
-Wyswietl:
+Loop2:
+cpi Sign, 13		; Subtract
+brne Loop3
+rjmp Subtract
 
-cpi Wynik,10
-brlt Jeden
+Loop3:
+cpi Sign, 16		; Multiply
+rjmp Multiply
 
-cpi Wynik,100
-brlt Dwa
 
-cpi Wynik,255   // było 100
-brlt Trzy
+Add:
+add Result, Number
+rcall Display
 
-Jeden:
-ldi WKolumna, 0b000
-rjmp Dalej			
+Subtract:
+sub Result, Number
+rcall Display
 
-Dwa:
-ldi WKolumna,3
-rjmp Dalej	
+Multiply:
+mul Result, Number
+rcall Display
 
-Trzy:
-ldi WKolumna,7	
 
-Dalej:
 
-out PORTC,WKolumna		;  portd zamiast (chyba) portu b
+Display:
+
+cpi Result,10
+brlt One
+
+cpi Result,100
+brlt Two
+
+cpi Result,255
+brlt Three
+
+One:
+ldi WColumn, 0b000
+rjmp Next
+
+Two:
+ldi WColumn,3
+rjmp Next
+
+Three:
+ldi WColumn,7
+
+Next:
+
+out PORTC, WColumn
 
 push R19
 
-Etykieta1:
-subi Wynik,100 // 1000
-brlo Etykieta2
+Tag1:
+subi Result,100 // 1000
+brlo Tag2
 inc R19
-rjmp Etykieta1
+rjmp Tag1
 
-Etykieta2:
-subi Wynik,-100  //-1000
-mov R26,R19  
+Tag2:
+subi Result,-100  //-1000
+mov R26,R19
 ldi R19,0
 
-Etykieta3:
-subi Wynik,100
-brlo Etykieta4
+Tag3:
+subi Result,100
+brlo Tag4
 inc R19
-rjmp Etykieta3
+rjmp Tag3
 
-Etykieta4:
-subi Wynik,-100
+Tag4:
+subi Result,-100
 mov R27,R19
 ldi R19,0
-rjmp Wyswietl
+rjmp Display
 
-Etykieta5:
-subi Wynik,10
-brlo Etykieta6
+Tag5:
+subi Result,10
+brlo Tag6
 inc R19
-rjmp Etykieta5
+rjmp Tag5
 
-Etykieta6:
-subi Wynik,-10
+Tag6:
+subi Result,-10
 mov R28,R19
 ldi R19,0
-rjmp Wyswietl
+rjmp Display
 
-mov R29, Wynik
+mov R29, Result
 pop R19
 
 
-//jeden  
+//One
 
-cpi R20, 0		breq  jedenzero
-cpi R20, 1		breq  jedenjeden
-cpi R20, 2		breq  jedendwa
-cpi R20, 3		breq  jedentrzy
-cpi R20, 4		breq  jedencztery
-cpi R20, 5		breq  jedenpiec
-cpi R20, 6		breq  jedenszesc
-cpi R20, 7		breq  jedensiedem
-cpi R20, 8		breq  jedenosiem
-cpi R20, 9		breq  jedendziewiec
+cpi R20, 0		breq  Onezero
+cpi R20, 1		breq  OneOne
+cpi R20, 2		breq  OneTwo
+cpi R20, 3		breq  OneThree
+cpi R20, 4		breq  OneFour
+cpi R20, 5		breq  OneFive
+cpi R20, 6		breq  OneSix
+cpi R20, 7		breq  OneSeven
+cpi R20, 8		breq  OneEight
+cpi R20, 9		breq  OneNine
 
-;wyświetlenie wyniku
-jedenzero:		ldi R20, 0b00111111		ret
-jedenjeden:		ldi R20, 0b00000110		ret
-jedendwa:		ldi R20, 0b01011011		ret
-jedentrzy:		ldi R20, 0b01001111		ret
-jedencztery:		ldi R20, 0b01100110		ret
-jedenpiec:		ldi R20, 0b01101101		ret
-jedenszesc:		ldi R20, 0b01111101		ret
-jedensiedem:		ldi R20, 0b00000111		ret
-jedenosiem:		ldi R20, 0b11111111		ret
-jedendziewiec:		ldi R20, 0b01101111		ret
+;displaying Result
 
-out PORTC,r20		; zapalanie odpowiednich segmentów
+Onezero:		ldi R20, 0b00111111		ret
+OneOne:		    ldi R20, 0b00000110		ret
+OneTwo:		    ldi R20, 0b01011011		ret
+OneThree:		ldi R20, 0b01001111		ret
+OneFour:		ldi R20, 0b01100110		ret
+OneFive:		ldi R20, 0b01101101		ret
+OneSix:		    ldi R20, 0b01111101		ret
+OneSeven:		ldi R20, 0b00000111		ret
+OneEight:		ldi R20, 0b11111111		ret
+OneNine:		ldi R20, 0b01101111		ret
 
-
-//dwa   // R0 na r26    jeśli równe = ładuj na wyświetlaczu
-
-cpi R26, 0		breq  dwazero 
-cpi R26, 1		breq  dwajeden
-cpi R26, 2		breq  dwadwa
-cpi R26, 3		breq  dwatrzy
-cpi R26, 4		breq  dwacztery
-cpi R26, 5		breq  dwapiec
-cpi R26, 6		breq  dwaszesc
-cpi R26, 7		breq  dwasiedem
-cpi R26, 8		breq  dwaosiem
-cpi R26, 9		breq  dwadziewiec
-
-dwazero:		ldi R26, 0b00111111		ret
-dwajeden:		ldi R26, 0b00000110		ret
-dwadwa:			ldi R26, 0b01011011		ret
-dwatrzy:		ldi R26, 0b01001111		ret
-dwacztery:		ldi R26, 0b01100110		ret
-dwapiec:		ldi R26, 0b01101101		ret
-dwaszesc:		ldi R26, 0b01111101		ret
-dwasiedem:		ldi R26, 0b00000111		ret	
-dwaosiem:		ldi R26, 0b11111111		ret
-dwadziewiec:		ldi R26, 0b01101111		ret
-
-out PORTC,R26	
+out PORTC,r20		; displaying rows
 
 
-//trzy   //R1 na R27
+//Two   // R0 -> r26   if equal = display
 
-cpi R27, 0		breq  trzyzero
-cpi R27, 1		breq  trzyjeden
-cpi R27, 2		breq  trzydwa
-cpi R27, 3		breq  trzytrzy
-cpi R27, 4		breq  trzycztery
-cpi R27, 5		breq  trzypiec
-cpi R27, 6		breq  trzyszesc
-cpi R27, 7		breq  trzysiedem
-cpi R27, 8		breq  trzyosiem
-cpi R27, 9		breq  trzydziewiec
+cpi R26, 0		breq  Twozero
+cpi R26, 1		breq  TwoOne
+cpi R26, 2		breq  TwoTwo
+cpi R26, 3		breq  TwoThree
+cpi R26, 4		breq  TwoFour
+cpi R26, 5		breq  TwoFive
+cpi R26, 6		breq  TwoSix
+cpi R26, 7		breq  TwoSeven
+cpi R26, 8		breq  TwoEight
+cpi R26, 9		breq  TwoNine
 
-trzyzero:		ldi R27, 0b00111111		ret
-trzyjeden:		ldi R27, 0b00000110		ret
-trzydwa:		ldi R27, 0b01011011		ret
-trzytrzy:		ldi R27, 0b01001111		ret	
-trzycztery:		ldi R27, 0b01100110		ret
-trzypiec:		ldi R27, 0b01101101		ret
-trzyszesc:		ldi R27, 0b01111101		ret	
-trzysiedem:		ldi R27, 0b00000111		ret	
-trzyosiem:		ldi R27, 0b11111111		ret
-trzydziewiec:		ldi R27, 0b01101111		ret
+Twozero:		ldi R26, 0b00111111		ret
+TwoOne:		    ldi R26, 0b00000110		ret
+TwoTwo:			ldi R26, 0b01011011		ret
+TwoThree:		ldi R26, 0b01001111		ret
+TwoFour:		ldi R26, 0b01100110		ret
+TwoFive:		ldi R26, 0b01101101		ret
+TwoSix:		    ldi R26, 0b01111101		ret
+TwoSeven:		ldi R26, 0b00000111		ret
+TwoEight:		ldi R26, 0b11111111		ret
+TwoNine:		ldi R26, 0b01101111		ret
 
-out PORTC,R27		
-
-
-//cztery		// R2 na R28 
-
-cpi R28, 0		breq  czteryzero
-cpi R28, 1		breq  czteryjeden
-cpi R28, 2		breq  czterydwa
-cpi R28, 3		breq  czterytrzy
-cpi R28, 4		breq  czterycztery
-cpi R28, 5		breq  czterypiec
-cpi R28, 6		breq  czteryszesc
-cpi R28, 7		breq  czterysiedem
-cpi R28, 8		breq  czteryosiem
-cpi R28, 9		breq czterydziewiec
-
-czteryzero:		ldi R28, 0b00111111		ret
-czteryjeden:		ldi R28, 0b00000110		ret
-czterydwa:		ldi R28, 0b01011011		ret
-czterytrzy:		ldi R28, 0b01001111		ret
-czterycztery:		ldi R28, 0b01100110		ret
-czterypiec:		ldi R28, 0b01101101		ret
-czteryszesc:		ldi R28, 0b01111101		ret
-czterysiedem:		ldi R28, 0b00000111		ret
-czteryosiem:		ldi R28, 0b11111111		ret
-czterydziewiec: 	ldi R28, 0b01101111		ret
-
-out PORTC,R28		
+out PORTC,R26
 
 
-//pięć   // R3 na R29
+//Three   //R1 -> R27
 
-cpi R29, 0		breq  pieczero		
-cpi R29, 1		breq  piecjeden
-cpi R29, 2		breq  piecdwa
-cpi R29, 3		breq  piectrzy
-cpi R29, 4		breq  pieccztery
-cpi R29, 5		breq  piecpiec
-cpi R29, 6		breq  piecszesc
-cpi R29, 7		breq  piecsiedem
-cpi R29, 8		breq  piecosiem
-cpi R29, 9		breq  piecdziewiec
+cpi R27, 0		breq  Threezero
+cpi R27, 1		breq  ThreeOne
+cpi R27, 2		breq  ThreeTwo
+cpi R27, 3		breq  ThreeThree
+cpi R27, 4		breq  ThreeFour
+cpi R27, 5		breq  ThreeFive
+cpi R27, 6		breq  ThreeSix
+cpi R27, 7		breq  ThreeSeven
+cpi R27, 8		breq  ThreeEight
+cpi R27, 9		breq  ThreeNine
 
-pieczero:       	ldi R29, 0b00111111		ret
-piecjeden:      	ldi R29, 0b00000110		ret
-piecdwa:        	ldi R29, 0b01011011		ret
-piectrzy:       	ldi R29, 0b01100110		ret
-piecpiec:       	ldi R29, 0b01101101		ret
-piecszesc:      	ldi R29, 0b01111101		ret
-piecsiedem:     	ldi R29, 0b00000111		ret
-piecosiem:      	ldi R29, 0b11111111		ret
-piecdziewiec:   	ldi R29, 0b01101111		ret
+Threezero:		ldi R27, 0b00111111		ret
+ThreeOne:		ldi R27, 0b00000110		ret
+ThreeTwo:		ldi R27, 0b01011011		ret
+ThreeThree:		ldi R27, 0b01001111		ret
+ThreeFour:		ldi R27, 0b01100110		ret
+ThreeFive:		ldi R27, 0b01101101		ret
+ThreeSix:		ldi R27, 0b01111101		ret
+ThreeSeven:		ldi R27, 0b00000111		ret
+ThreeEight:		ldi R27, 0b11111111		ret
+ThreeNine:		ldi R27, 0b01101111		ret
 
-out PORTC,R29		
+out PORTC,R27
+
+
+//Four		// R2 -> R28
+
+cpi R28, 0		breq  Fourzero
+cpi R28, 1		breq  FourOne
+cpi R28, 2		breq  FourTwo
+cpi R28, 3		breq  FourThree
+cpi R28, 4		breq  FourFour
+cpi R28, 5		breq  FourFive
+cpi R28, 6		breq  FourSix
+cpi R28, 7		breq  FourSeven
+cpi R28, 8		breq  FourEight
+cpi R28, 9		breq FourNine
+
+Fourzero:		ldi R28, 0b00111111		ret
+FourOne:		ldi R28, 0b00000110		ret
+FourTwo:		ldi R28, 0b01011011		ret
+FourThree:		ldi R28, 0b01001111		ret
+FourFour:		ldi R28, 0b01100110		ret
+FourFive:		ldi R28, 0b01101101		ret
+FourSix:		ldi R28, 0b01111101		ret
+FourSeven:		ldi R28, 0b00000111		ret
+FourEight:		ldi R28, 0b11111111		ret
+FourNine: 	    ldi R28, 0b01101111		ret
+
+out PORTC,R28
+
+
+//five   // R3 -> R29
+
+cpi R29, 0		breq  Fivezero
+cpi R29, 1		breq  FiveOne
+cpi R29, 2		breq  FiveTwo
+cpi R29, 3		breq  FiveThree
+cpi R29, 4		breq  FiveFour
+cpi R29, 5		breq  FiveFive
+cpi R29, 6		breq  FiveSix
+cpi R29, 7		breq  FiveSeven
+cpi R29, 8		breq  FiveEight
+cpi R29, 9		breq  FiveNine
+
+Fivezero:       	ldi R29, 0b00111111		ret
+FiveOne:      	    ldi R29, 0b00000110		ret
+FiveTwo:        	ldi R29, 0b01011011		ret
+FiveThree:       	ldi R29, 0b01100110		ret
+FiveFive:       	ldi R29, 0b01101101		ret
+FiveSix:      	    ldi R29, 0b01111101		ret
+FiveSeven:     	    ldi R29, 0b00000111		ret
+FiveEight:      	ldi R29, 0b11111111		ret
+FiveNine:   	    ldi R29, 0b01101111		ret
+
+out PORTC,R29
 ret
 
 
-BUZZ_on:			;włączenie buzzera
+BUZZ_on:			; buzzer on
 cbi PORTD, 0
 
 
-czekaj1: ldi R26, 2
-czekaj2: ldi R27, 10
-czekaj3: ldi R28, 255
+wait1: ldi R26, 2
+wait2: ldi R27, 10
+wait3: ldi R28, 255
 //nop
-//nop 
-dec R28 brne czekaj3
-dec R27 brne czekaj2
-dec R26 brne czekaj1
+//nop
+dec R28 brne wait3
+dec R27 brne wait2
+dec R26 brne wait1
 
 sbi  PORTD, 0
 ret
