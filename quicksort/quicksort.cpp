6688df86cdea1c271eb21b5b75c80597918a1629
Krzych <michnic120@gmail.com>
2019-12-16 21:56:54 +0100

    task description added

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -16,3 +16,26 @@ void quicksort(int tab[], int left, int right){
      if(left<j) quicksort(tab,left,j);
      if(right>i) quicksort(tab,i,right);     
 }
+
+
+
+/*
+Zadanie BK16. Sortowanie liczb — metoda quicksort
+
+
+Algorytm sortowania szybkiego (ang. quicksort) jest bardzo wydajny: jego złożoność jest rzędu
+O(n log n). Metoda stosuje podejście rekurencyjne — dziel i rządź. 
+
+Wybieramy pewien element tablicy, tzw. element dzielący, i na początek tablicy przenosimy wszystkie elementy
+mniejsze od niego, na koniec wszystkie większe, a w powstałe między tymi obszarami pu-
+ste miejsce trafia wybrany element. 
+
+Potem sortujemy osobno początkową i końcową część
+tablicy. Rekursja kończy się, gdy kolejny fragment uzyskany z podziału zawiera pojedyn-
+czy element, jako że jednoelementowa podtablica jest posortowana. 
+
+Należy zaprojektować
+odpowiedni algorytm podziału na P procesorów/maszyn, opracować program, wykonać te-
+sty i przedstawić analizę wyników. 
+
+*/
