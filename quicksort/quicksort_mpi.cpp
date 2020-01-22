fb4f43d61e43f9626971986b166f8b57552e2d77
Krzych <michnic120@gmail.com>
2020-01-22 19:56:36 +0100

    mpi implementation finished

--- quicksort/quicksort_mpi.cpp
+++ quicksort/quicksort_mpi.cpp
@@ -14,24 +14,16 @@
 
 using itVec = std::vector<int>::iterator;
 
-
-inline double timeSum(const std::vector<double>& timeVec, const int& n)
-{
-    return std::accumulate(timeVec.begin(), timeVec.end(), 0.0000) / (CLOCKS_PER_SEC * n);
-}
-
-
 int main(int argc, char* argv[])
 {
-    const int size = 20, m = 1000; //1024;
+    const int root = 0, size = 20, m = 1000;
     int numOfProc, myRank;
-
     double clock1;
     std::vector<int> vec(size);
+    
     std::generate(vec.begin(), vec.end(), []() {return rand() % m;});
 
     MPI_Init(&argc, &argv);
-
     MPI_Comm_size(MPI_COMM_WORLD, &numOfProc);
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
@@ -43,18 +35,16 @@ int main(int argc, char* argv[])
 
     if (myRank == 0)
     {
-        std::cout << "Number of available processors: " << numOfProc << "\n" ;
-
-        // std::cout << "\n General before sort: \n\t";
-        // for (const auto& iter : vec)
-        // {
-        //     std::cout << iter << " ";
-        // }
+        std::cout << "Available processors: " << numOfProc << "\n" 
+                  << "\n General before sort: \n\t";
+        for (const auto& iter : vec)
+        {
+            std::cout << iter << " ";
+        }
     }
 
     int indexBegin = I_BEGIN(size, myRank, numOfProc);
     int indexEnd = I_END(size, myRank, numOfProc);
-    const int root = 0;
     int lenSum = 0;
 
     itVec iterBegin = vec.begin() + indexBegin;
@@ -68,45 +58,37 @@ int main(int argc, char* argv[])
     std::vector<int> recvIndex(numOfProc);
     std::vector<int> gatherVec(size);
     std::vector<int> partVec(iterBegin, iterEnd);
-    std::vector<int> gatherRegSam;
+    std::vector<int> gatherRegSamples;
 
     clock1 = MPI_Wtime();
-
+    
     std::sort(partVec.begin(), partVec.end());
 
- 
     for (int i = 0; i < numOfProc; i++)
     {
-        regularSamples[i] = *(iterBegin + (i * size / pow(numOfProc, 2)));
-
-        if (myRank == root) std::cout << "\n\tregularSamples : " << regularSamples[i] ;
+        regularSamples[i] = *(iterBegin + (i * size / pow(numOfProc, 2))-1);
     }
-   
- 
+
     if (myRank == root)
-    {
-        gatherRegSam.resize(pow(numOfProc, 2));
+    {   
+        gatherRegSamples.resize(pow(numOfProc, 2));
     }
-
 
     MPI_Gather(regularSamples.data(), numOfProc, MPI_INT,
-            gatherRegSam.data(), numOfProc, MPI_INT, root,
-            MPI_COMM_WORLD);
-
+            gatherRegSamples.data(), numOfProc, MPI_INT, 
+            root, MPI_COMM_WORLD);
 
     if (myRank == root)
     {
-        std::sort(gatherRegSam.begin(), gatherRegSam.end());
+        std::sort(gatherRegSamples.begin(), gatherRegSamples.end());
 
         for (int i = 0; i < numOfProc-1; i++)
         {
-            privots[i] = gatherRegSam[(i+1)*numOfProc];
+            privots[i] = gatherRegSamples[(i+1)*numOfProc];
         }
     }
 
-
     MPI_Bcast(privots.data(), numOfProc, MPI_INT, root, MPI_COMM_WORLD);
-
 
     auto j = 0u;
     for (int i = 0; i<numOfProc-1; i++)
@@ -123,12 +105,9 @@ int main(int argc, char* argv[])
     sendIndex[numOfProc-1] = j;
     sendLength[numOfProc-1] = partVec.size() - j;
 
-
-
     MPI_Alltoall(sendLength.data(), 1, MPI_INT,
                  recvLength.data(), 1, MPI_INT,
                  MPI_COMM_WORLD);
-
 
     for (int i = 0; i < numOfProc; i++)
     {
@@ -137,40 +116,38 @@ int main(int argc, char* argv[])
     }
     std::vector<int> recvVec(lenSum);
 
-
     MPI_Alltoallv(partVec.data(), sendLength.data(), sendIndex.data(), MPI_INT,
                  recvVec.data(), recvLength.data(), recvIndex.data(), MPI_INT,
-                MPI_COMM_WORLD);
-
-
+                 MPI_COMM_WORLD);
 
     std::sort(recvVec.begin(), recvVec.end());
 
+    auto count = lenSum;
+    MPI_Gather(&count, 1, MPI_INT,
+            recvLength.data(), 1, MPI_INT, 
+            root, MPI_COMM_WORLD);
+    
+    lenSum = 0;
+    for (int i = 0; i < numOfProc; i++)
+    {
+        recvIndex[i] = lenSum;
+        lenSum += recvLength[i];
+    }
 
-
-
-    MPI_Gather(recvVec.data(), lenSum, MPI_INT,
-            gatherVec.data(), lenSum, MPI_INT, root,
-            MPI_COMM_WORLD);
-
-
+    MPI_Gatherv(recvVec.data(), count, MPI_INT,
+            vec.data(), recvLength.data(), recvIndex.data(), MPI_INT, 
+            root, MPI_COMM_WORLD);
 
     clock1 = MPI_Wtime() - clock1;
 
-    // if (myRank == root && i == 0)
-    // {
-    //     std::cout << "\n Gathered after sort: \n\t";
-    //     for (const auto& iter : gatherVec)
-    //     {
-    //          std::cout << iter << " ";
-    //     }
-
-    // }
-
-
     if (myRank == root)
     {
-        std::cout << "\n Tp: " << clock1 << "\n";
+        std::cout << "\n\n Gathered after sort: \n\t";
+        for (const auto& iter : vec)
+        {
+             std::cout << iter << " ";
+        }
+        std::cout << "\n\n Processing time: \n\t" << clock1 << "\n";
     }
 
     MPI_Finalize();
